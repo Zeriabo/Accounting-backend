2d97dfe46fee4ce85e4125acbd8ba9b43d0f2332
Zeriab <zeriab@hotmail.com>
2020-10-18 15:22:55 +0300

    adding Acess control

--- server.js
+++ server.js
@@ -986,6 +986,22 @@ app.get("/getTrailBalance",async function(req,res){
         
 
 app.use(function (err, req, res, next) {
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+   res.setHeader('Access-Control-Allow-Origin', 'https://zaccounting.netlify.app/');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
   console.error(err.message);
   if (!err.statusCode) err.statusCode = 500;
   res.status(err.statusCode).send(err.message);
