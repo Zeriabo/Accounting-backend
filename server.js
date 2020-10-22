b7b8d918677aed5dcf36a8af99d0e60128802d66
Zeriab <zeriab@hotmail.com>
2020-10-22 18:53:06 +0300

    revert

--- server.js
+++ server.js
@@ -1,45 +1,23 @@
 let express = require('express');
 var path = require("path");  
 var app = express(); 
-
 let mongoose = require('mongoose');
 let cors = require('cors');
 let bodyParser = require('body-parser');
 let dbConfig = require('./database/db');
 const axios = require("axios");
 const { exit } = require('process');
-const http = require("http");
-const socketIo = require("socket.io");
+const http = require("http")
+const server = http.createServer(app);
+
+const io = require("socket.io")(server);
 const index = require("./routes/index");
 app.use(index);
-const server = http.createServer(app);
-const io = socketIo(server); 
-
-const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get(
-      "https://api.darksky.net/forecast/PUT_YOUR_API_KEY_HERE/43.7695,11.2558"
-    ); // Getting the data from DarkSky
-    socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
-};
 
 
 
 
-let interval;
-io.on("connection", socket => {
-  console.log("New client connected");
-  if (interval) {
-    clearInterval(interval);
-  }
-  interval = setInterval(() => getApiAndEmit(socket), 10000);
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
+
 
 
 app.use(cors());
@@ -87,7 +65,10 @@ const port = process.env.PORT || 4000;
 
 //listening on socket 
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+// server.listen(port, () => console.log(`Listening on port ${port}`));
+// io.on("connection", function(socket) {
+//   console.log("socket connected");
+//  });
 
 const Schema = mongoose.Schema;
 var ResultBalanceSchema = new Schema({
