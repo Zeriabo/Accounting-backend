42df46920b02ab791f6f034f405dffb977800722
Zeriab Chiah <zeriab@hotmail.com>
2023-01-09 11:55:32 +0200

    DB

--- database/db.js
+++ database/db.js
@@ -1,4 +1,7 @@
 module.exports = {
-  db: //"mongodb://localhost:27017/Accounting"
-       "mongodb+srv://zeriab:XShyLV0Z3pT2RMH8@accounting.dfuow.gcp.mongodb.net/Accounting?retryWrites=true&w=majority"
+  //"mongodb://localhost:27017/Accounting"
+  db:
+    "mongodb+srv://zeriab:" +
+    process.env.DB_PASSWORD +
+    "@accounting.dfuow.gcp.mongodb.net/Accounting?retryWrites=true&w=majority",
 };
